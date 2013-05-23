6827fbd4973b2b206a97139e5c96e171c69e1891
aleksgor <agorshkov@griddynamics.com>
2013-05-23 16:14:42 +0400

    add tests

--- cli/src/test/org/apache/hadoop/hive/cli/TestCliDriverMethods.java
+++ cli/src/test/org/apache/hadoop/hive/cli/TestCliDriverMethods.java
@@ -17,19 +17,6 @@
  */
 package org.apache.hadoop.hive.cli;
 
-import junit.framework.TestCase;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.metastore.api.FieldSchema;
-import org.apache.hadoop.hive.metastore.api.Schema;
-import org.apache.hadoop.hive.ql.CommandNeedRetryException;
-import org.apache.hadoop.hive.ql.Driver;
-import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
-
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -38,6 +25,28 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import jline.ArgumentCompletor;
+import jline.Completor;
+import jline.ConsoleReader;
+import junit.framework.TestCase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.hadoop.hive.metastore.api.Schema;
+import org.apache.hadoop.hive.ql.CommandNeedRetryException;
+import org.apache.hadoop.hive.ql.Driver;
+import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
 
 // Cannot call class TestCliDriver since that's the name of the generated
 // code for the script-based testing
@@ -58,7 +67,8 @@ public class TestCliDriverMethods extends TestCase {
   }
 
   // If the command has no schema, make sure nothing is printed
-  public void testThatCliDriverPrintsNoHeaderForCommandsWithNoSchema() throws CommandNeedRetryException {
+  public void testThatCliDriverPrintsNoHeaderForCommandsWithNoSchema()
+      throws CommandNeedRetryException {
     Schema mockSchema = mock(Schema.class);
     when(mockSchema.getFieldSchemas()).thenReturn(null);
 
@@ -69,16 +79,20 @@ public class TestCliDriverMethods extends TestCase {
 
   /**
    * Do the actual testing against a mocked CliDriver based on what type of schema
-   * @param mockSchema Schema to throw against test
+   *
+   * @param mockSchema
+   *          Schema to throw against test
    * @return Output that would have been sent to the user
-   * @throws CommandNeedRetryException won't actually be thrown
+   * @throws CommandNeedRetryException
+   *           won't actually be thrown
    */
   private PrintStream headerPrintingTestDriver(Schema mockSchema) throws CommandNeedRetryException {
     CliDriver cliDriver = new CliDriver();
 
     // We want the driver to try to print the header...
     Configuration conf = mock(Configuration.class);
-    when(conf.getBoolean(eq(ConfVars.HIVE_CLI_PRINT_HEADER.varname), anyBoolean())).thenReturn(true);
+    when(conf.getBoolean(eq(ConfVars.HIVE_CLI_PRINT_HEADER.varname), anyBoolean()))
+        .thenReturn(true);
     cliDriver.setConf(conf);
 
     Driver proc = mock(Driver.class);
@@ -99,4 +113,102 @@ public class TestCliDriverMethods extends TestCase {
     return mockOut;
   }
 
+
+  public void testGetCommandCompletor() {
+    Completor[] completors = CliDriver.getCommandCompletor();
+    assertEquals(2, completors.length);
+    assertTrue(completors[0] instanceof ArgumentCompletor);
+    assertTrue(completors[1] instanceof Completor);
+  }
+
+  public void testRun() throws Exception {
+
+    CliSessionState ss = new CliSessionState(new HiveConf());
+    CliSessionState.start(ss);
+    String[] args = {};
+    PrintStream oldOut = System.out;
+    ByteArrayOutputStream dataOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(dataOut));
+
+    PrintStream oldErr = System.err;
+    ByteArrayOutputStream dataErr = new ByteArrayOutputStream();
+    System.setErr(new PrintStream(dataErr));
+
+
+    try {
+      new FakeCliDriver().run(args);
+    } finally {
+      System.setOut(oldOut);
+      System.setErr(oldErr);
+
+    }
+    assertTrue(dataOut.toString().contains("test message"));
+    assertTrue(dataErr.toString().contains("Hive history file="));
+    assertTrue(dataErr.toString().contains("File: fakeFile is not a file."));
+
+    System.out.println("1----------------------");
+    System.out.println("" + dataOut.toString());
+    System.out.println("2----------------------");
+    System.out.println("" + dataErr.toString());
+    System.out.println("3----------------------" + dataErr.toString().length());
+  }
+
+  private static class FakeCliDriver extends CliDriver {
+
+    @Override
+    protected ConsoleReader getConsoleReader() throws IOException {
+      ConsoleReader reslt = new FakeConsoleReader();
+      return reslt;
+    }
+
+  }
+
+  private static class FakeConsoleReader extends ConsoleReader {
+    private int counter = 0;
+    File temp = null;
+
+    public FakeConsoleReader() throws IOException {
+      super();
+
+    }
+
+    @Override
+    public String readLine(String prompt) throws IOException {
+      FileWriter writer;
+      switch (counter++) {
+      case 0:
+        return "!echo test message;";
+      case 1:
+        temp = File.createTempFile("hive", "test");
+        temp.deleteOnExit();
+        return "source  " + temp.getAbsolutePath() + ";";
+      case 2:
+        temp = File.createTempFile("hive", "test");
+        temp.deleteOnExit();
+        writer = new FileWriter(temp);
+        writer.write("bla bla bla");
+        writer.close();
+        return "list file file://" + temp.getAbsolutePath() + ";";
+      case 3:
+        return "!echo ";
+      case 4:
+        return "test message;";
+      case 5:
+        return "source  fakeFile;";
+      case 6:
+        temp = File.createTempFile("hive", "test");
+        temp.deleteOnExit();
+        writer = new FileWriter(temp);
+        writer.write("source  fakeFile;");
+        writer.close();
+        return "list file file://" + temp.getAbsolutePath() + ";";
+
+
+        // drop table over10k;
+      default:
+        return null;
+      }
+    }
+
+  }
 }
